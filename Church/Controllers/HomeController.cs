14d5d4ae6c606aa15c6666b27a4fc28c95aeda11
Lawrence M <lawrence.muthian@xpo.com>
2020-04-19 16:27:22 +0200

    Added the Church API folders

---
+++ Church/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Church.Controllers
+{
+    [Route("church/home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Church", "Malankara Catholic Church" };
+        }
+    }
+}
---
+++ ChurchTest/Service/HomeServiceTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChurchTest.Service
+{
+    class HomeServiceTest
+    {
+    }
+}
